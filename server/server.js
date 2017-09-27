eb3603ae75c804f52efec46e501476886087d164
Swan <swan@yakoo.com.hk>
2017-09-27 15:41:28 +0800

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.delete('/todos/:id', (req, res) => {
         if (!todo) {
             return res.status(404).send();
         }
-        res.status(200).send(todo);
+        res.status(200).send({todo});
     }).catch(error => {
         res.status(400).send();
     })
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -112,3 +112,39 @@ describe('GET /todos/:id', () => {
             .end(done);
     })
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', done => {
+        request(app)
+            .delete(`/todos/${todos[1]._id.toHexString()}`)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toBe(todos[1].text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                }
+                Todo.findById(res.body.todo._id).then(todo => {
+                    expect(todo).toBe(null);
+                    done();
+                }).catch(err => {
+                    done(err);
+                })
+            })
+    });
+
+    it('should return 404 if todo not found', done => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if object ID is invalid', done => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
