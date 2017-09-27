747383db4b5db7804dea829bc8bf3c5c547fe38d
Swan <swan@yakoo.com.hk>
2017-09-27 09:47:57 +0800

    add GET /todos test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,8 +5,15 @@ const { app } = require('../server');
 const { Todo } = require('../models/todo');
 // const { User } = require('../models/user');
 
+const todos = [
+    { text: 'First test todo' },
+    { text: 'Second test todo' }
+];
+
 beforeEach(done => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,7 +33,7 @@ describe('POST /todos', () => {
                 if (err) {
                     return done(err);
                 }
-                Todo.find().then(todos => {
+                Todo.find({ text }).then(todos => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -47,7 +54,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
                 Todo.find({}).then(todos => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch(err => {
                     done(err);
@@ -55,3 +62,20 @@ describe('POST /todos', () => {
             })
     });
 });
+
+describe('GET /todos', () => {
+    it('should get all todos', done => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                }
+                done();
+            });
+    })
+})
