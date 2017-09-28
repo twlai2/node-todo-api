7fd9a62307551f4b69ec1284ec7b758523b6441a
Swan <swan@yakoo.com.hk>
2017-09-28 17:09:14 +0800

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,26 +4,12 @@ const request = require('supertest');
 
 const { app } = require('../server');
 const { Todo } = require('../models/todo');
-// const { User } = require('../models/user');
+const { User } = require('../models/user');
 
-const todos = [
-    { 
-        _id: new ObjectID(),
-        text: 'First test todo' 
-    },
-    { 
-        _id: new ObjectID(),
-        text: 'Second test todo',
-        completed: true,
-        completedAt: 333
-    }
-];
+const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
-beforeEach(done => {
-    Todo.remove({}).then(() => {
-        return Todo.insertMany(todos);
-    }).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
     it('should create a new todo', done => {
@@ -190,3 +176,73 @@ describe('PATCH /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('GET /users/me', () => {
+    it('should return user if authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect(res => {
+                expect(res.body._id).toBe(users[0]._id.toHexString());
+                expect(res.body.email).toBe(users[0].email);                
+            })
+            .end(done);
+    });
+
+    it('should return 401 if not authenticated', (done) => {
+        request(app)
+            .get('/users/me')
+            .expect(401)
+            .expect(res => {
+                expect(res.body).toEqual({});
+            })
+            .end(done);
+    })
+});
+
+describe('POST /users', () => {
+    it('should create a user', done => {
+        const email = 'test@yakoo.com.hk';
+        const password = 'userTestPassword';
+        request(app)
+            .post('/users')
+            .send({ email, password })
+            .expect(200)
+            .expect(res => {
+                expect(res.headers['x-auth']).toBeTruthy();
+                expect(res.body.user._id).toBeTruthy();
+                expect(res.body.user.email).toBe(email);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findOne({ email }).then(user => {
+                    expect(user).toBeTruthy();
+                    expect(user.password).not.toBe(password);
+                    done();
+                })
+            });
+    });
+
+    it('should return validation error if request is invalid', done => {
+        const email = 'test@yakoo.com.hk';
+        const password = 'pa';
+        request(app)
+            .post('/users')
+            .send({ email, password })
+            .expect(400)
+            .end(done);
+    })
+
+    it('should not create user if email is in use', done => {
+        const email = users[0].email;
+        const password = 'userOnePass';
+        request(app)
+            .post('/users')
+            .send({ email, password })
+            .expect(400)
+            .end(done);
+    })
+})
