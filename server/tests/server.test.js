c70889cae81893982f28fa4cf9d6565ac09b6abb
Swan <swan@yakoo.com.hk>
2017-09-29 17:15:36 +0800

    Add test route POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -245,4 +245,52 @@ describe('POST /users', () => {
             .expect(400)
             .end(done);
     })
-})
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', done => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect(res => {
+                expect(res.headers['x-auth']).toBeTruthy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then(user => {
+					expect(user.tokens).toHaveLength(1);
+                    expect(user.tokens[0].access).toBe('auth');
+                    expect(user.tokens[0].token).toBe(res.headers['x-auth']);
+                    done();
+                }).catch(e => done(e));
+            })
+    });
+
+    it('should reject invalid login', done => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + '1'
+            })
+            .expect(401)
+            .expect(res => {
+                expect(res.headers['x-auth']).not.toBeTruthy();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then(user => {
+                    expect(user.tokens).toHaveLength(0);
+                    done();
+                })
+            })
+    });
+});
