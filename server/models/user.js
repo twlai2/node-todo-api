306c56127850f3ddb5e2f9c1a400b2a0d01ea363
Swan <swan@yakoo.com.hk>
2017-09-29 16:35:00 +0800

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -69,6 +69,30 @@ UserSchema.statics.findByToken = function(token) {
     });
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+    const User = this;
+    return User.findOne({
+        email
+    }).then(user => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (err) {
+                    reject();
+                }
+                if (!res) {
+                    reject();
+                }
+                resolve(user);
+            })
+        });
+    }).catch(() => {
+        return Promise.reject();
+    })
+}
+
 UserSchema.pre('save', function(next) {
     const user = this;
     if (user.isModified('password')) {
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { ObjectID } = require('mongodb');
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -106,6 +107,17 @@ app.post('/users', (req, res) => {
     .catch(err => {
         res.status(400).send(err);
     });
+});
+
+app.post('/users/login', (req, res) => {
+    const { email, password } = req.body;
+    User.findByCredentials(email, password).then(user => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch(() => {
+        res.status(401).send();
+    })
 });
 
 app.get('/users/me', authenticate, (req, res) => {
