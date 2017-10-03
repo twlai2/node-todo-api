35fff63e479042d1e3225b3f0feff8ea6d8facc5
Swan <swan@yakoo.com.hk>
2017-10-03 09:46:29 +0800

    Add DELETE /users/me/token and test

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,17 @@ UserSchema.methods.generateAuthToken = function() {
     });
 }
 
+UserSchema.methods.removeToken = function(token) {
+    const user = this;
+    return user.update({
+        $pull: {
+            tokens: {
+                token
+            }
+        }
+    })
+}
+
 UserSchema.statics.findByToken = function(token) {
     const User = this;
     let decoded;
